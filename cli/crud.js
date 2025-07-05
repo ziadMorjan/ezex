956c6093c00a2eeef41fc0fadcd84a4e46dba54b
Ziad Morjan <zmorjan765@gmail.com>
2025-07-05 22:27:58 +0300

    fix order of creating crud

--- cli/crud.js
+++ cli/crud.js
@@ -1,13 +1,13 @@
-const {createController}=require("./utils/crud/createController");
-const {createRouter}=require("./utils/crud/createRouter");
-const {createModel}=require("./utils/crud/createModel");
-const {addApp}=require("./templates/addToApp")
-const {name}=require("./utils/naming")
+const { createController } = require("./utils/crud/createController");
+const { createRouter } = require("./utils/crud/createRouter");
+const { createModel } = require("./utils/crud/createModel");
+const { addApp } = require("./templates/addToApp")
+const { name } = require("./utils/naming")
 
-exports.createCRUD=async(projectDir,crudName)=>{
-    const parsedName=name(crudName)
-    await createController(projectDir,parsedName)
-    await createRouter(projectDir,parsedName)
-    await createModel(projectDir,parsedName)
-    addApp(projectDir,{crudName:parsedName})
+exports.createCRUD = async (projectDir, crudName) => {
+    const parsedName = name(crudName)
+    await createModel(projectDir, parsedName)
+    await createController(projectDir, parsedName)
+    await createRouter(projectDir, parsedName)
+    addApp(projectDir, { crudName: parsedName })
 }
